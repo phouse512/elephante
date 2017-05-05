0207cdac83c2807b070748e6e1629eccd9efcfb6
phouse512 <philiphouse2015@u.northwestern.edu>
2017-05-05 15:53:05 -0700

    adding tags

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { render } from 'react-dom';
+
+import Fuse from 'fuse.js';
 
 class Results extends React.Component {
 	constructor(props) {
@@ -10,6 +11,40 @@ class Results extends React.Component {
 		this.state = {
 			results: props.results
 		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({results: nextProps.results});
+	}
+
+	jumpToUrl(event, url) {
+		chrome.tabs.query({currentWindow: true, active: true}, (tab) => {
+
+			chrome.storage.sync.get('elephante_stats', (items) => {
+				var stats_obj;
+				var elephante_stats;
+				if (items.elephante_stats == null) {
+					elephante_stats = {};
+				} else {
+					elephante_stats = items.elephante_stats;
+				}
+
+				if (!('retrievals' in elephante_stats)) {
+					// if this is first time, instantiate an empty object
+					stats_obj = {};
+					stats_obj[url] = 1;
+				} else {
+					stats_obj = elephante_stats.retrievals;
+					stats_obj[url] = (stats_obj[url] || 0) + 1;
+				}
+
+				elephante_stats['retrievals'] = stats_obj;
+				chrome.storage.sync.set({'elephante_stats': elephante_stats}, () => {
+					console.log(stats_obj);
+					chrome.tabs.update(tab.id, {url: url});
+				});
+			});
+		});
 	}
 
 	render() {
@@ -23,7 +58,7 @@ class Results extends React.Component {
 		} else {
 			results = this.state.results.map(search => {
 				return (
-					<div className="result">
+					<div onClick={(e) => this.jumpToUrl(e, search.url)} className="result">
 						<h4>{search.title}</h4>
 					</div>
 				)
@@ -63,13 +98,36 @@ export default class App extends React.Component {
 			url: '',
 			title: '',
 			view: 'search',
-			placeholderText: 'search all snippets'
+			placeholderText: 'search all snippets',
+			search_result: []
 		};
 
 		this.handlePress = this.handlePress.bind(this);
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSave = this.handleSave.bind(this);
-		this.submit = this.submit.bind(this);
+		this.submitButton = this.submitButton.bind(this);
+		chrome.storage.sync.get('elephante_urls', (items) => {
+			if ('items' in items.elephante_urls) {
+				this.setState({list: items.elephante_urls.items});
+			} else {
+				this.setState({list: []});
+			}
+
+			this.state.search_options = {
+				shouldSort: true,
+				threshold: 0.6,
+				location: 0,
+				distance: 100,
+				maxPatternLength: 32,
+				minMatchCharLength: 1,
+				keys: [
+					"title",
+					"url",
+					"tags"
+				]
+			};
+			this.state.fuse = new Fuse(this.state.list, this.state.search_options);
+		});
 	}
 
 	componentDidMount() {
@@ -81,43 +139,72 @@ export default class App extends React.Component {
 	}
 
 	handleChange(event) {
-		this.setState({value: event.target.value});
+		let val = event.target.value;
+		let result = this.state.fuse.search(val);
+		this.setState({value: val, search_result: result});
 	}
 
 	handlePress(event) {
-		console.log(event);
-		// if (event.key === 's') {
-		// 	this.setState({
-		// 		view: 'save', 
-		// 		title: document.title, 
-		// 		url: window.location.href,
-		// 		placeholderText: 'add tags here'
-		// 	});
-		// }
 		if (event.key === 'Enter') {
-			this.submit();
+			this.submitButton();
 		}
 	}
 
-	submit() {
-		if (this.state == 'save') {
+	submitButton() {
+		var title = this.state.title;
+		var url = this.state.url;
+		var tags = this.state.value.split(/[ ,]+/);
+		if (this.state.view == 'save') {
 			// saving logic here
 			console.log('saving what we have');
+			chrome.storage.sync.get('elephante_urls', (items) => {
+
+				var new_obj;
+				if (!('items' in items.elephante_urls)) {
+					console.log('new stuff');
+					new_obj = {
+						items: [
+							{
+								title: title,
+								url: url,
+								tags: tags
+							}
+						]
+					}
+				} else {
+					console.log('pushing to existing');
+					new_obj = {
+						items: items.elephante_urls.items
+					}
+					new_obj.items.push({
+						title: title,
+						url: url,
+						tags: tags
+					});
+				}
+
+				this.state.list = new_obj.items;
+				this.state.fuse = new Fuse(this.state.list, this.state.search_options);
+				chrome.storage.sync.set({'elephante_urls': new_obj}, () => {
+					console.log(new_obj);
+					console.log('success');
+					this.setState({value: '', view: 'search', url: '', title: '', placeholderText: 'search all snippets'});
+				});
+			});
 		} else {
 			console.log('nothing to do');
 		}
 	}
 
 	handleSave(event) {
-		var state = this;
-		chrome.tabs.getSelected(null, function(tab) {
-			state.setState({
+		chrome.tabs.getSelected(null, (tab) => {
+			this.setState({
 				view: 'save',
 				placeholderText: 'add tags here',
 				url: tab.url,
 				title: tab.title
-			})
-		})
+			});
+		});
 	}
 
 	render() {
@@ -126,15 +213,18 @@ export default class App extends React.Component {
 			bottom = <AddTags title={this.state.title}
 						url={this.state.url} />;
 		} else {
-			bottom = <Results results={[]}/>;
+			console.log('results in app render');
+			console.log(this.state.search_result);
+			bottom = <Results results={this.state.search_result}/>;
 		}
+
 		return (
 			<div>
 				<div className="searchContainer">
 					<div className="saveButton" onClick={this.handleSave}>
 						<i className="fa fa-plus" aria-hidden="true"></i>
 					</div>
-					<input className="searchBar" placeholder={this.state.placeholderText} onChange={this.handleChange} type="text" />
+					<input className="searchBar" placeholder={this.state.placeholderText} onChange={this.handleChange} type="text" value={this.state.value} />
 				</div>
 				{ bottom }
 			</div>
